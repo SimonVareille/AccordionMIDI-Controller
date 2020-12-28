a752273d8525da936b4d331241f73ff5f6c7cdb9
Simon Vareille <si.vareille@gmail.com>
2020-12-28 13:42:43 +0100

    Add midiio module

---
+++ src/core/arduino/io/midiio.py
@@ -0,0 +1,27 @@
+import mido
+
+class MidiIO:
+    def __init__(self):
+        pass
+
+    def _process_sysex(self, data):
+        pass
+
+    def connect(self):
+        pass
+
+    def connect_input(self):
+        pass
+
+    def connect_output(self):
+        pass
+
+    def close(self):
+        pass
+
+    def send_sysex(self, data):
+        pass
+
+    def recv_sysex(self):
+        pass
+
