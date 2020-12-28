7d13971055c8a8a4a091486975e6777ccb2d0df3
Simon Vareille <si.vareille@gmail.com>
2020-12-28 12:54:47 +0100

    Implements Left96ButtonKeyboard

--- src/core/keyboard/keyboard.py
+++ src/core/keyboard/keyboard.py
@@ -53,13 +53,61 @@ class Left96ButtonKeyboard(Keyboard):
     """Left keyboard.
 
     Represent a left button keyboard of 96 button, as 6 rows of 16 buttons.
+
+    Format of this keyboard is as:
+    |96  95  94  93  92  91  90  89  88  87  86  85  84  83  82  81
+    |  80  79  78  77  76  75  74  73  72  71  70  69  68  67  66  65
+    |    64  63  62  61  60  59  58  57  56  55  54  53  52  51  50  49
+    |      48  47  46  45  44  43  42  41  40  39  38  37  36  35  34  33
+    |        32  31  30  29  28  27  26  25  24  23  22  21  20  19  18  17
+    |          16  15  14  13  12  11  10   9   8   7   6   5   4   3   2   1
     """
 
+    def __init__(self, name=None):
+        super().__init__(name)
+        self.keyboard = [None, ]*96
+
+    def __repr__(self):
+        return "Left96ButtonKeyboard(name={!r})".format(self.name)
+
     def set_data(self, identifier, data):
-        """See Keyboard.setData."""
+        """
+        Set the value of the data identified by `identifier`.
+
+        Parameters
+        ----------
+        identifier : int
+            index of the data to set (1-96).
+        data : MidiData
+            data to set.
+
+        Returns
+        -------
+        None.
+
+        """
+        if not isinstance(data, MidiData):
+            raise TypeError(
+                "data must be of type MidiData (or subclasses), not {}"
+                .format(type(data)))
+        self.keyboard[identifier-1] = data
 
     def get_data(self, identifier):
-        """See Keyboard.getData."""
+        """
+        Get the value of the data identified by `identifier`.
+
+        Parameters
+        ----------
+        identifier : int
+            index of the data to get (1-96).
+
+        Returns
+        -------
+        MidiData
+            The wanted data.
+
+        """
+        return self.keyboard[identifier-1]
 
 
 class Right81ButtonKeyboard(Keyboard):
