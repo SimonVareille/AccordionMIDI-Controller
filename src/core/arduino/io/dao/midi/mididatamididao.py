9a7d36827ea5dcc2155f323359aecc58b3ff588b
Simon Vareille <si.vareille@gmail.com>
2021-01-09 18:26:36 +0100

    Add NoteDataMidiDAO class

--- src/core/arduino/io/dao/midi/mididatamididao.py
+++ src/core/arduino/io/dao/midi/mididatamididao.py
@@ -1,7 +1,7 @@
 """Definition of MidiData's DAO using MIDI protocol."""
 
 from abc import ABC, abstractmethod
-from core.keyboard import MidiData
+from core.keyboard import MidiData, NoteData
 
 
 class MidiDataMidiDAO(ABC):
@@ -9,7 +9,7 @@ class MidiDataMidiDAO(ABC):
 
     @staticmethod
     @abstractmethod
-    def from_bytes(data: bytearray) -> MidiData:
+    def from_bytes(data: bytearray) -> (MidiData, int):
         """
         Create a MidiData from SysEx bytes.
 
@@ -21,6 +21,36 @@ class MidiDataMidiDAO(ABC):
         Returns
         -------
         MidiData
-            The created MidiData, or None if not possible.
+            The created MidiData and the , or None if not possible.
+        int
+            The number of bytes read.
+        """
+
+
+class NoteDataMidiDAO(MidiDataMidiDAO):
+    """Represents a NoteData using midi."""
+
+    @staticmethod
+    def from_bytes(data: bytearray) -> (NoteData, int):
+        """
+        Create a NoteData from SysEx bytes.
+
+        Parameters
+        ----------
+        data : bytearray
+            Part of the SysEx message containing the NoteData.
+
+        Returns
+        -------
+        NoteData
+            The created NoteData or None if not possible.
+        int
+            The number of bytes read.
 
         """
+        if data[0] == 0x01:
+            channel = data[1]
+            pitch = data[2]
+            velocity = data[3]
+            return NoteData(channel, pitch, velocity), 4
+        return None, 0
