c6cae9f9341b0a1284b0881b972dee3efb78d235
Simon Vareille <si.vareille@gmail.com>
2021-01-17 11:57:47 +0100

    Add MidiDAOFactory

--- src/core/arduino/io/dao/daofactory.py
+++ src/core/arduino/io/dao/daofactory.py
@@ -1,6 +1,7 @@
 """Factory for the DAOs."""
 from abc import ABC, abstractmethod
-from .keyboarddao import KeyboardDAO
+from .keyboarddao import KeyboardDAO, Left96ButtonKeyboardMidiDAO,\
+    Right81ButtonKeyboardMidiDAO
 
 
 class DAOFactory(ABC):
@@ -9,8 +10,9 @@ class DAOFactory(ABC):
     def __init__(self):
         pass
 
+    @staticmethod
     @abstractmethod
-    def get_left_96_button_keyboard_dao(self) -> KeyboardDAO:
+    def get_left_96_button_keyboard_dao() -> KeyboardDAO:
         """
         Return the appropriate Left96ButtonKeyboard's DAO.
 
@@ -21,8 +23,9 @@ class DAOFactory(ABC):
 
         """
 
+    @staticmethod
     @abstractmethod
-    def get_right_81_button_keyboard_dao(self) -> KeyboardDAO:
+    def get_right_81_button_keyboard_dao() -> KeyboardDAO:
         """
         Return the appropriate Right81ButtonKeyboard's DAO.
 
@@ -32,3 +35,33 @@ class DAOFactory(ABC):
             The Right81ButtonKeyboard's DAO.
 
         """
+
+
+class MidiDAOFactory(DAOFactory):
+    """Factory for creating KeyboardMidiDAO."""
+
+    @staticmethod
+    def get_left_96_button_keyboard_dao() -> Left96ButtonKeyboardMidiDAO:
+        """
+        Return the Left96ButtonKeyboardMidiDAO.
+
+        Returns
+        -------
+        Left96ButtonKeyboardMidiDAO
+            The Left96ButtonKeyboardMidiDAO.
+
+        """
+        return Left96ButtonKeyboardMidiDAO()
+
+    @staticmethod
+    def get_right_81_button_keyboard_dao() -> Right81ButtonKeyboardMidiDAO:
+        """
+        Return the Right81ButtonKeyboardMidiDAO.
+
+        Returns
+        -------
+        Right81ButtonKeyboardMidiDAO
+            The Right81ButtonKeyboardMidiDAO.
+
+        """
+        return Right81ButtonKeyboardMidiDAO()
