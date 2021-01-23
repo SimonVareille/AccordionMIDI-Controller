0340e7ced591cfcfbd470395818b6174c6509022
Simon Vareille <si.vareille@gmail.com>
2021-01-23 14:00:28 +0100

    Add origin module

---
+++ src/core/origin.py
@@ -0,0 +1,17 @@
+"""Enumeration of the possible origin of each keyboard.
+
+It's intended to differenciate keyboards comming from Arduino, from a local
+database or from a remote DB.
+"""
+
+from enum import Enum
+
+
+class Origin(Enum):
+    """Enumeration of the possible keyboards's origin.
+
+    The current possibilities are:
+        - Arduino
+    """
+
+    Arduino = 1
