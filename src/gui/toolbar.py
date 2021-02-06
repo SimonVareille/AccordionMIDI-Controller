ff8fccc504cfb39725038d75c9fa1d7536a2104a
Simon Vareille <si.vareille@gmail.com>
2021-02-06 11:52:03 +0100

    Add internationalisation of ToolBar

--- src/gui/toolbar.py
+++ src/gui/toolbar.py
@@ -1,6 +1,6 @@
 """Toolbar of the controller."""
 
-from PyQt5.QtWidgets import QToolBar, QAction
+from PyQt5.QtWidgets import QToolBar, QAction, QApplication
 from PyQt5.QtGui import QIcon
 
 
@@ -8,30 +8,42 @@ class ToolBar(QToolBar):
     """The toolbar of the controller."""
 
     def __init__(self, owner, *args, **kwargs):
-        super().__init__("Keyboard actions", owner, *args, **kwargs)
-        self.pull_action = QAction(QIcon('icons/download-button'),
-                                   'Pull keyboards from accordion', owner)
-        self.create_keyboard_action = QAction(QIcon('icons/round-add-button'),
-                                              'Create new keyboard', owner)
-        self.clone_keyboard_action = QAction(QIcon('icons/copy-content'),
-                                             'Clone current keyboard', owner)
+        super().__init__(
+            QApplication.translate("ToolBar", "Keyboard actions"),
+            owner, *args, **kwargs)
+        self.pull_action = QAction(
+            QIcon('icons/download-button'),
+            self.tr('Pull keyboards from accordion'),
+            owner)
+        self.create_keyboard_action = QAction(
+            QIcon('icons/round-add-button'),
+            self.tr('Create new keyboard'),
+            owner)
+        self.clone_keyboard_action = QAction(
+            QIcon('icons/copy-content'),
+            self.tr('Clone current keyboard'),
+            owner)
         self.delete_keyboard_action = QAction(
             QIcon('icons/round-delete-button'),
-            'Delete current keyboard', owner)
+            self.tr('Delete current keyboard'), owner)
         self.apply_keyboard_action = QAction(
-            QIcon('icons/send-button'), 'Apply current keyboard on accordion',
+            QIcon('icons/send-button'),
+            self.tr('Apply current keyboard on accordion'),
             owner)
         self.store_keyboard_action = QAction(
             QIcon('icons/save-button'),
-            'Store current keyboard on accordion',
+            self.tr('Store current keyboard on accordion'),
             owner)
         self.apply_and_store_keyboard_action = QAction(
             QIcon('icons/upload-button'),
-            'Apply and store current keyboard on accordion', owner)
+            self.tr('Apply and store current keyboard on accordion'), owner)
         self.reset_keyboard_action = QAction(
             QIcon('icons/undo-button.svg'),
-            'Reset current keyboard', owner)
-        self.about_action = QAction(QIcon('icons/help.svg'), 'About', owner)
+            self.tr('Reset current keyboard'), owner)
+        self.about_action = QAction(
+            QIcon('icons/help.svg'),
+            self.tr('About'),
+            owner)
 
         # disable toolbuttons that needs input or output port initialized
         self.pull_action.setEnabled(False)
