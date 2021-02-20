07f5c2069fa9d67e0cdd48da99b974801ff072a1
Simon Vareille <si.vareille@gmail.com>
2021-02-20 14:43:14 +0100

    Add Actions class and usage

--- src/gui/controller.py
+++ src/gui/controller.py
@@ -4,15 +4,17 @@ Icons made by <a href="https://www.flaticon.com/authors/google"
 title="Google">Google</a> from <a href="https://www.flaticon.com/"
 title="Flaticon"> www.flaticon.com</a>
 """
-import sys
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import os
+
+from PyQt5.QtCore import QSize, Qt, QObject, QCoreApplication
+from PyQt5.QtWidgets import QMainWindow, QAction, QFileDialog
+from PyQt5.QtGui import QIcon
 
 from core import ControllerCore
-from core.origin import Origin
 
 from .toolbar import ToolBar
-from .keyboardselection import KeyboardSelection
+from .keyboardselection import KeyboardFileSelection
+import gui.resources
 
 
 class ControllerGUI(QMainWindow):
@@ -24,52 +26,96 @@ class ControllerGUI(QMainWindow):
         self.setMinimumSize(QSize(640, 480))
         self.setWindowTitle("Controller")
 
+        self.base_path = os.getcwd()
+
         # Controller core
         self.controller = ControllerCore()
 
+        # Actions
+        self.actions = Actions(self)
+
+        # Menu bar
+        menubar = self.menuBar()
+        file_menu = menubar.addMenu(self.tr('&File'))
+        file_menu.addAction(self.actions.new)
+        file_menu.addAction(self.actions.open)
+        file_menu.addSeparator()
+        file_menu.addAction(self.actions.exit)
+
         # Tool bar
 
-        self.toolbar = ToolBar(self)
+        self.toolbar = ToolBar(self, self.actions)
         self.addToolBar(Qt.TopToolBarArea, self.toolbar)
-        # self.connect_toolbar()
 
         # Keyboard selection dock
-        self.keyboard_selection = KeyboardSelection(self)
+        self.keyboard_selection = KeyboardFileSelection(self)
         self.keyboard_selection.setAllowedAreas(
             Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
         self.addDockWidget(Qt.LeftDockWidgetArea, self.keyboard_selection)
 
-        self.populate_keyboard_selection_model()
+        # self.populate_keyboard_selection_model()
+
+        self.connect_actions()
 
         self.statusBar().showMessage(self.tr("Initialization done"))
 
-    def connect_toolbar(self):
+    def connect_actions(self):
         """
-        Connect the toolbar signals with internal slots.
+        Connect the QActions signals with internal slots.
 
         Returns
         -------
         None.
 
         """
-        self.toolbar.pull_action.triggered.connect(
-            self.pull_layouts)
-        self.toolbar.create_keyboard_action.triggered.connect(
-            self.create_layout)
-        self.toolbar.clone_keyboard_action.triggered.connect(
-            self.clone_layout)
-        self.toolbar.delete_keyboard_action.triggered.connect(
-            self.delete_layout)
-        self.toolbar.apply_keyboard_action.triggered.connect(
-            self.apply_layout)
-        self.toolbar.store_keyboard_action.triggered.connect(
-            self.store_layout)
-        self.toolbar.apply_and_store_keyboard_action.triggered.connect(
-                self.apply_and_store_layout)
-        self.toolbar.reset_keyboard_action.triggered.connect(
-            self.reset_layout)
-        self.toolbar.about_action.triggered.connect(
-            self.about_dialog)
+        self.actions.exit.triggered.connect(self.close)
+        self.actions.open.triggered.connect(self.open_file_dialog)
+        # self.actions.pull.triggered.connect(
+        #     self.pull_layouts)
+        # self.actions.create_keyboard.triggered.connect(
+        #     self.create_layout)
+        # self.actions.clone_keyboard.triggered.connect(
+        #     self.clone_layout)
+        # self.actions.delete_keyboard.triggered.connect(
+        #     self.delete_layout)
+        # self.actions.apply_keyboard.triggered.connect(
+        #     self.apply_layout)
+        # self.actions.store_keyboard.triggered.connect(
+        #     self.store_layout)
+        # self.actions.apply_and_store_keyboard.triggered.connect(
+        #         self.apply_and_store_layout)
+        # self.actions.reset_keyboard.triggered.connect(
+        #     self.reset_layout)
+        # self.actions.about.triggered.connect(
+        #     self.about_dialog)
+
+    def close(self):
+        """
+        Close the applicaiton.
+
+        Returns
+        -------
+        None.
+
+        """
+        QCoreApplication.quit()
+
+    def open_file_dialog(self):
+        """
+        Open the file dialog.
+
+        Returns
+        -------
+        None.
+
+        """
+        filename = QFileDialog.getOpenFileName(
+            self,
+            self.tr("Open Keyboard"),
+            self.base_path,
+            self.tr("Keyboard Files (*.json)"))[0]
+        keyboard_state = self.controller.open(filename)
+        # TODO: Create a new pannel with the returned keyboard_state
 
     def populate_keyboard_selection_model(self):
         """
@@ -86,3 +132,71 @@ class ControllerGUI(QMainWindow):
             for kbd in lst_kbds:
                 self.keyboard_selection.selection_model.add_keyboard(
                     origin, kbd)
+
+
+class Actions(QObject):
+    """Contains every QAction the GUI can use."""
+
+    def __init__(self, owner):
+        super().__init__(owner)
+        # Exit
+        self.exit = QAction(
+            QIcon(':/icons/power-off-solid.svg'),
+            self.tr('&Exit'),
+            owner)
+        self.exit.setShortcut('Ctrl+Q')
+        self.exit.setStatusTip(self.tr('Exit application'))
+        # Open
+        self.open = QAction(
+            QIcon(':/icons/folder-symbol.svg'),
+            self.tr('&Open'),
+            owner)
+        self.open.setShortcut('Ctrl+O')
+        self.open.setStatusTip(self.tr('Open a file'))
+        # New
+        self.new = QAction(
+            QIcon(':/icons/google-drive-file.svg'),
+            self.tr('Create new keyboard'),
+            owner)
+        self.new.setShortcut('Ctrl+N')
+        self.new.setStatusTip(self.tr('Create a new keyboard'))
+        # Pull from Arduino
+        self.pull = QAction(
+            QIcon(':/icons/download-button.svg'),
+            self.tr('Pull keyboards from accordion'),
+            owner)
+        # Clone
+        self.clone_keyboard = QAction(
+            QIcon(':/icons/copy-content.svg'),
+            self.tr('Clone current keyboard'),
+            owner)
+        # Delete
+        self.delete_keyboard = QAction(
+            QIcon(':/icons/round-delete-button.svg'),
+            self.tr('Delete current keyboard'),
+            owner)
+        # Apply on Arduino
+        self.apply_keyboard = QAction(
+            QIcon(':/icons/send-button.svg'),
+            self.tr('Apply current keyboard on accordion'),
+            owner)
+        # Store on Arduino
+        self.store_keyboard = QAction(
+            QIcon(':/icons/save-button.svg'),
+            self.tr('Store current keyboard on accordion'),
+            owner)
+        # Apply and store on Arduino
+        self.apply_and_store_keyboard = QAction(
+            QIcon(':/icons/upload-button.svg'),
+            self.tr('Apply and store current keyboard on accordion'),
+            owner)
+        # Reset
+        self.reset_keyboard = QAction(
+            QIcon(':/icons/undo-button.svg'),
+            self.tr('Reset current keyboard'),
+            owner)
+        # Help
+        self.about = QAction(
+            QIcon(':/icons/help.svg'),
+            self.tr('About'),
+            owner)
--- src/gui/toolbar.py
+++ src/gui/toolbar.py
@@ -3,59 +3,28 @@
 from PyQt5.QtWidgets import QToolBar, QAction, QApplication
 from PyQt5.QtGui import QIcon
 
+import gui.resources
+
 
 class ToolBar(QToolBar):
     """The toolbar of the controller."""
 
-    def __init__(self, owner, *args, **kwargs):
+    def __init__(self, owner, actions, *args, **kwargs):
         super().__init__(
             QApplication.translate("ToolBar", "Keyboard actions"),
             owner, *args, **kwargs)
-        self.pull_action = QAction(
-            QIcon('icons/download-button'),
-            self.tr('Pull keyboards from accordion'),
-            owner)
-        self.create_keyboard_action = QAction(
-            QIcon('icons/round-add-button'),
-            self.tr('Create new keyboard'),
-            owner)
-        self.clone_keyboard_action = QAction(
-            QIcon('icons/copy-content'),
-            self.tr('Clone current keyboard'),
-            owner)
-        self.delete_keyboard_action = QAction(
-            QIcon('icons/round-delete-button'),
-            self.tr('Delete current keyboard'), owner)
-        self.apply_keyboard_action = QAction(
-            QIcon('icons/send-button'),
-            self.tr('Apply current keyboard on accordion'),
-            owner)
-        self.store_keyboard_action = QAction(
-            QIcon('icons/save-button'),
-            self.tr('Store current keyboard on accordion'),
-            owner)
-        self.apply_and_store_keyboard_action = QAction(
-            QIcon('icons/upload-button'),
-            self.tr('Apply and store current keyboard on accordion'), owner)
-        self.reset_keyboard_action = QAction(
-            QIcon('icons/undo-button.svg'),
-            self.tr('Reset current keyboard'), owner)
-        self.about_action = QAction(
-            QIcon('icons/help.svg'),
-            self.tr('About'),
-            owner)
 
         # disable toolbuttons that needs input or output port initialized
-        self.pull_action.setEnabled(False)
+        actions.pull.setEnabled(False)
 
         # populate toolbar
-        self.addAction(self.pull_action)
-        self.addAction(self.create_keyboard_action)
-        self.addAction(self.clone_keyboard_action)
-        self.addAction(self.delete_keyboard_action)
-        self.addAction(self.reset_keyboard_action)
-        self.addAction(self.apply_keyboard_action)
-        self.addAction(self.store_keyboard_action)
-        self.addAction(self.apply_and_store_keyboard_action)
+        self.addAction(actions.pull)
+        self.addAction(actions.new)
+        self.addAction(actions.clone_keyboard)
+        self.addAction(actions.delete_keyboard)
+        self.addAction(actions.reset_keyboard)
+        self.addAction(actions.apply_keyboard)
+        self.addAction(actions.store_keyboard)
+        self.addAction(actions.apply_and_store_keyboard)
         self.addSeparator()
-        self.addAction(self.about_action)
+        self.addAction(actions.about)
